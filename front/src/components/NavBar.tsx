8fcbf4d912de766af165434a4cd51266f66374a1
Nicolas Rabello <nic.rabello@gmail.com>
2024-04-10 01:53:39 -0300

    Created book registration form

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 "use client"
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const router = useRouter();
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -10,8 +12,8 @@ export default function NavBar() {
 
     return (
         <nav className="bg-teal-500 border-gray-700 h-16 border-y">
-            <div className="max-w-fit flex flex-wrap items-center justify-center mx-auto p-4 h-full">
-                <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <div className="max-w-fit flex flex-wrap items-center justify-center mx-auto p-4 h-full cursor cursor-pointer">
+                <a onClick={() => router.push("/") } className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span className="self-center text-3xl font-semibold whitespace-nowrap text-teal-900">Livros</span>
                 </a>
             </div>
